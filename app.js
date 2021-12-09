15aa29aa4335065c98f2fcf3a1588293bcca5b8f
maku <makucode@gmail.com>
2021-12-09 13:44:57 +0100

    cors fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
+import cors from "cors";
 import dbConnect from "./config/db.js";
 import routes from "./config/routes.js";
 import error from "./middleware/error.js";
@@ -11,6 +12,7 @@ dbConnect();
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 routes(app);
 app.use(error);
